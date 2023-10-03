a90131ec0f102152302e8a18cb698a9891da1480
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-03 23:55:58 +0200

    Book Buttons SweetAlert2

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -17,22 +17,22 @@ function Book({ books }: BookProps) {
     alert(text);
   }*/
 
-  const SwalDialog = (titulo: string) => {
+  const SwalDialog = (titulo: string, imagem: string) => {
     Swal.fire({
       title: 'Atenção',
-      text: "Deseja comprar o livro?",
-      icon: 'question',
+      html: "<img src='" + imagem + "' /><br>Deseja comprar o livro?",
+      //icon: 'question',
       showCancelButton: true,
       confirmButtonColor: '#0d6efd',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Comprar',      
+      confirmButtonText: 'Comprar',
       cancelButtonText: 'Cancelar'
     }).then((result) => {
       if (result.isConfirmed) {
 
         Swal.fire({
-          title: 'Comprado!',
-          html: 'Transação do livro: <strong>'+ titulo + '<br></strong> foi Efetuada com Sucesso',
+          title: 'Perfeito!',
+          html: 'Transação do livro: <strong>' + titulo + '<br></strong> foi Efetuada com Sucesso',
           icon: 'success',
           confirmButtonColor: '#0d6efd',
           confirmButtonText: 'OK'
@@ -44,19 +44,19 @@ function Book({ books }: BookProps) {
   return (
     <>
       {books.map((book) => (
-          <div key={book.title} className='col-md-4 pt-5 text-center'>
-            <h5>{book.title}</h5>
-            <p>{book.author}</p>
-            <p>{book.year}</p>
-            <p>{book.genre}</p>
-            <img src={book.image} />
+        <div key={book.title} className='col-md-4 pt-5 text-center'>
+          <h5>{book.title}</h5>
+          <p>{book.author}</p>
+          <p>{book.year}</p>
+          <p>{book.genre}</p>
+          <img src={book.image} />
 
           <div className='p-5 d-grid gap-2'>
             <Botao
-              click={() => SwalDialog(book.title)} // book.year error
+              click={() => SwalDialog(book.title, book.image)} // book.year error
               tipo='btn btn-primary'
             >
-              <strong>{book.title}</strong>
+              <strong>COMPRAR</strong>
             </Botao>
           </div>
         </div>

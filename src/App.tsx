cda7ccd74d203f96e1857131f01193a5d31e4b75
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-03 15:41:09 +0200

    Clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import './App.css'
-import Header from './components/header'
-import Session from './components/Session'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Session/>
     </>
   )
 }

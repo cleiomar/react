16ba57360c4fe028259aaf529094e63268a0b899
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-03 23:26:11 +0200

    Book Buttons SweetAlert2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import Books from './components/Books'
+import Swal from 'sweetalert2'
 
 function App() {
+  
   return (
     <>
         <div className='container'>
--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,4 +1,5 @@
 import Botao from "./Botao";
+import Swal from 'sweetalert2';
 
 type BookProps = {
   books: {
@@ -12,25 +13,47 @@ type BookProps = {
 
 function Book({ books }: BookProps) {
 
-  const handleClick2 = (text: string) => {
+  /*const handleClick2 = (text: string) => {
     alert(text);
+  }*/
+
+  const SwalDialog = (titulo: string) => {
+    Swal.fire({
+      title: 'Atenção',
+      text: "Deseja comprar o livro?",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#0d6efd',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Comprar',      
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        Swal.fire({
+          title: 'Comprado!',
+          html: 'Transação do livro: <strong>'+ titulo + '<br></strong> foi Efetuada com Sucesso',
+          icon: 'success',
+          confirmButtonColor: '#0d6efd',
+          confirmButtonText: 'OK'
+        })
+      }
+    })
   }
 
   return (
     <>
       {books.map((book) => (
-        <div key={book.title} className='col-md-4 p-5 text-center'>
-          <div>
-            <h2>{book.title}</h2>
+          <div key={book.title} className='col-md-4 pt-5 text-center'>
+            <h5>{book.title}</h5>
             <p>{book.author}</p>
             <p>{book.year}</p>
             <p>{book.genre}</p>
             <img src={book.image} />
-          </div>
 
           <div className='p-5 d-grid gap-2'>
             <Botao
-              click={() => handleClick2(book.author)} // book.year error
+              click={() => SwalDialog(book.title)} // book.year error
               tipo='btn btn-primary'
             >
               <strong>{book.title}</strong>

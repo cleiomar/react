445812cb517aebe42a5fb26702bc45fd1536b087
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-05 01:13:18 +0200

    Att Sweetalert2 Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
+import Data from "./Data";
+
 
 function App() {
+  const [valor, setValor] = useState(1);
+  const [imagem, setImagem] = useState('https://a-static.mlcdn.com.br/1500x1500/livro-a-sociedade-do-anel-capa-os-aneis-de-poder/magazineluiza/235381700/435e208634bd6a1344a4065ba0da771c.jpg');
 
+  const pegarImagem = (id) => {
 
-  const [valor, setValor] = useState(0);
-
+    const found = Data.find(obj => {
+      return obj.id === id;
+    });
+    return found['image'];
+  }
+  
+  
   const handleClickPreview = () => {
-    if (valor > 0) {
+    if (valor > 1) {
       setValor(valor - 1)
+      setImagem(pegarImagem(valor-1))
     }
   }
 
   const handleClickNext = () => {
     if (valor < 10) {
       setValor(valor + 1)
+      setImagem(pegarImagem(valor+1))
     }
   }
 
   return (
+    
     <>
       <div className='container'>
-      <div className='row'>
-          <div className='col-md-4'>
-            Teste
+        <div className='row pt-5 imgCenter'>
+          <div className='images'>
+          <img src={imagem} />
+
           </div>
-          <div>
-            <button onClick={handleClickPreview}>-</button>
-            <span> {valor} </span>
-            <button onClick={handleClickNext}>+</button>
+        </div>
+        <div className='row pt-5'>
+          <div className='buttonClick'>
+            <button className='btn btn-primary btn-qnt' onClick={handleClickPreview}>Preview</button>
+            <span className='span-qnt'> {valor} </span>
+            <button className='btn btn-primary btn-qnt' onClick={handleClickNext}>Next</button>
           </div>
         </div>
       </div>
---
+++ src/Data.ts
@@ -0,0 +1,44 @@
+const Data = [
+    {
+        id: 1,
+        image: "https://a-static.mlcdn.com.br/1500x1500/livro-a-sociedade-do-anel-capa-os-aneis-de-poder/magazineluiza/235381700/435e208634bd6a1344a4065ba0da771c.jpg"
+    },
+    {
+        id: 2,
+        image: "https://m.media-amazon.com/images/I/81ibfYk4qmL.jpg"
+    },
+    {
+        id: 3,
+        image: "https://m.media-amazon.com/images/I/51-DZ1v5gSL._SX343_BO1,204,203,200_.jpg"
+    },
+    {
+        id: 4,
+        image: "https://m.media-amazon.com/images/I/71Xta4Nf7uL.jpg"
+    },
+    {
+        id: 5,
+        image: "https://m.media-amazon.com/images/I/71abp7PqBSL.jpg"
+    },
+    {
+        id: 6,
+        image: "https://m.media-amazon.com/images/I/91+1SUO3vUL.jpg"
+    },
+    {
+        id: 7,
+        image: "https://m.media-amazon.com/images/I/61L+4OBhm-L.jpg"
+    },
+    {
+        id: 8,
+        image: "https://m.media-amazon.com/images/I/91qVTRzBNgL.jpg"
+    },
+    {
+        id: 9,
+        image: "https://m.media-amazon.com/images/I/51nNwwVSclL.jpg"
+    },
+    {
+        id: 10,
+        image: "https://m.media-amazon.com/images/I/81k74t+lqyL.jpg"
+    },
+]
+
+export default Data;

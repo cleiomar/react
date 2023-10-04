e486ffb4a773f2633b0d931c121143c6d3a9a127
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-04 18:55:59 +0200

    Novo Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+
+  const [valor, setValor] = useState(0);
+
+  const handleClickPreview = () => {
+    if (valor > 0) {
+      setValor(valor - 1)
+    }
+  }
+
+  const handleClickNext = () => {
+    if (valor < 10) {
+      setValor(valor + 1)
+    }
+  }
+
   return (
     <>
+      <div className='container'>
+      <div className='row'>
+          <div className='col-md-4'>
+            Teste
+          </div>
+          <div>
+            <button onClick={handleClickPreview}>-</button>
+            <span> {valor} </span>
+            <button onClick={handleClickNext}>+</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

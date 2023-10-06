a2808d0c221555bcbb33134aa0b6fe05b0caf8bf
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-06 16:22:41 +0200

    Formulario funcional

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import './App.css'
 
 function App() {
 
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [schooling, setSchooling] = useState('Médio');
   const [resume, setResume] = useState('');
-  
+
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert('Feito com sucesso!');
+    clearForm();
+  }
+
+  const clearForm = () => {
+    setName('');
+    setEmail('');
+    setSchooling('Médio');
+    setResume('');
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <label>
           nome:
@@ -21,6 +33,7 @@ function App() {
             onChange={({ target }) => setName(target.value)}
           />
         </label>
+        <br></br>
 
         <label>
           Email:
@@ -29,24 +42,28 @@ function App() {
             onChange={({ target }) => setEmail(target.value)}
           />
         </label>
+        <br></br>
 
         <label>
           Escolaridade:
-          <select value={schooling} onChange={({target}) => setSchooling(target.value)}>
+          <select value={schooling} onChange={({ target }) => setSchooling(target.value)}>
             <option value='Médio'>Médio</option>
             <option value='Superior'>Superior</option>
             <option value='Pós-Graduação'>Pós-Graduação</option>
           </select>
         </label>
+        <br></br>
+
         <label>
           Resumo das suas experiencias:
           <textarea
-          value={resume}
-          onChange={({target}) => setResume(target.value)}          
+            value={resume}
+            onChange={({ target }) => setResume(target.value)}
           />
-          
-        </label>
+          <br></br>
 
+        </label>
+        <button>Enviar</button>
       </form>
     </>
   )

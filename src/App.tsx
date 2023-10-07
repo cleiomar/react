4dd676ca786ff71e19d3c85a249406690a623b2a
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-07 19:15:58 +0200

    Inicio Sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { PropsWithChildren, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from './store';
+import { toggleRTL, toggleTheme, toggleLocale, toggleMenu, toggleLayout, toggleAnimation, toggleNavbar, toggleSemidark } from './store/themeConfigSlice';
+import store from './store';
 
+function App({ children }: PropsWithChildren) {
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const dispatch = useDispatch();
 
-function App() {
+    useEffect(() => {
+        dispatch(toggleTheme(localStorage.getItem('theme') || themeConfig.theme));
+        dispatch(toggleMenu(localStorage.getItem('menu') || themeConfig.menu));
+        dispatch(toggleLayout(localStorage.getItem('layout') || themeConfig.layout));
+        dispatch(toggleRTL(localStorage.getItem('rtlClass') || themeConfig.rtlClass));
+        dispatch(toggleAnimation(localStorage.getItem('animation') || themeConfig.animation));
+        dispatch(toggleNavbar(localStorage.getItem('navbar') || themeConfig.navbar));
+        dispatch(toggleLocale(localStorage.getItem('i18nextLng') || themeConfig.locale));
+        dispatch(toggleSemidark(localStorage.getItem('semidark') || themeConfig.semidark));
+    }, [dispatch, themeConfig.theme, themeConfig.menu, themeConfig.layout, themeConfig.rtlClass, themeConfig.animation, themeConfig.navbar, themeConfig.locale, themeConfig.semidark]);
 
-  const [formInfo, setFormInfo] = useState({
-    name: '',
-    email: '',
-    schooling: 'Médio',
-    resume: ''
-  });
-
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(formInfo);
-    clearForm();
-  }
-
-  const clearForm = () => {
-    setFormInfo({
-      name: '',
-      email: '',
-      schooling: 'Médio',
-      resume: ''
-    });
-  }
-
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.target;
-    setFormInfo({
-      ...formInfo,
-      [name]: value,
-    });
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-
-        <label>
-          nome:
-          <input
-            value={formInfo.name}
-            name='name'
-            onChange={handleChange}
-          />
-        </label>
-        <br></br>
-
-        <label>
-          Email:
-          <input
-            value={formInfo.email}            
-            name='email'
-            onChange={handleChange}
-          />
-        </label>
-        <br></br>
-
-        <label>
-          Escolaridade:
-          <select value={formInfo.schooling} name='schooling' onChange={handleChange}>
-            <option value='Médio'>Médio</option>
-            <option value='Superior'>Superior</option>
-            <option value='Pós-Graduação'>Pós-Graduação</option>
-          </select>
-        </label>
-        <br></br>
-
-        <label>
-          Resumo das suas experiencias:
-          <textarea
-            value={formInfo.resume}
-            name='resume'
-            onChange={handleChange}
-          />
-          <br></br>
-
-        </label>
-        <button>Enviar</button>
-      </form>
-    </>
-  )
+    return (
+        <div
+            className={`${(store.getState().themeConfig.sidebar && 'toggle-sidebar') || ''} ${themeConfig.menu} ${themeConfig.layout} ${
+                themeConfig.rtlClass
+            } main-section antialiased relative font-nunito text-sm font-normal`}
+        >
+            {children}
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Perfect Scrollbar
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+// Tailwind css
+import './tailwind.css';
+
+// i18n (needs to be bundled)
+import './i18n';
+
+// Router
+import { RouterProvider } from 'react-router-dom';
+import router from './router/index';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store/index';
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <Suspense>
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
+        </Suspense>
+    </React.StrictMode>
+);
+

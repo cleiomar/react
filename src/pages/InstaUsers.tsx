5bc212ff65e3074507b4e3e49e3fefdf92ba097d
Cleiomar Alves <cleiomar@hotmail.com>
2023-10-09 15:37:08 +0200

    Sistema Atualizado 09-10 v2

--- src/pages/InstaUsers.tsx
+++ src/pages/InstaUsers.tsx
@@ -1,5 +1,6 @@
 import DashBox from '../components/Dashboard/DashBox';
 import IconMenuInstaUsers from '../components/Icon/Menu/IconMenuInstaUsers';
+import IconSettings from '../components/Icon/IconSettings';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
@@ -11,21 +12,39 @@ const Dashboard = () => {
                 <ul className="flex space-x-2 rtl:space-x-reverse pb-5">
                     <li>
                         <Link to="/" className="text-primary hover:underline">
-                            {t('dashboard')}
+                            {t('accounts')}
                         </Link>
                     </li>
                     <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
-                        <span>Sales</span>
+                        <span>Instagram</span>
                     </li>
                 </ul>
                 <div className="flex gap-5 relative sm:h-[calc(100vh_-_150px)] h-full">
                     <div className="panel flex-1 overflow-auto h-full">
                         <div className="sm:min-h-[300px] min-h-[400px] p-5">
 
-
+                        <div className="grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 pb-5">
+                                <div className="panel pb-4 bg-primary-light shadow-primary">
+                                    <div className="min-h-[40px]">
+                                        <div className="flex justify-center">
+                                            <div className="ltr:ml-2 rtl:mr-2">
+                                                <IconMenuInstaUsers
+                                                    opValor='0.3'
+                                                    width='60'
+                                                    height='60'
+                                                />
+                                            </div>
+                                            <div className="ltr:ml-2 rtl:mr-2 w-[150px] self-end-baseline">
+                                                <div className="font-bold text-lg font-general text-gray-500">Accounts</div>
+                                                <div className="font-num font-semibold text-gray-500 ">35</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <ul className="font-bold ft titulo text-gray-500 pb-10 pt-0">
                                 <li>
-                                    {t('activity_report')}
+                                    {t('accounts')}
                                 </li>
                             </ul>
                             <div className="grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
@@ -37,31 +56,28 @@ const Dashboard = () => {
                                                 <img className="w-8 h-8 rounded-full border-2 border-white/50 block object-cover ltr:mr-1 rtl:ml-1" src="/assets/images/profile-34.jpeg" alt="avatar" />
                                                 Alan Green
                                             </div>
-                                            <button type="button" className="ltr:ml-auto rtl:mr-auto flex items-center justify-between w-9 h-9 bg-black text-white rounded-md hover:opacity-80">
-                                                <IconMenuInstaUsers className="w-6 h-6 m-auto" />
-                                            </button>
                                         </div>
                                         <div className="text-white flex justify-between items-center">
-                                            <p className="text-xl">Wallet Balance</p>
+                                            <p className="text-xl">Total Interactions</p>
                                             <h5 className="ltr:ml-auto rtl:mr-auto text-2xl">
-                                                <span className="text-white-light">$</span>2953
+                                                <span className="text-xl text-white-light">2953</span>
                                             </h5>
                                         </div>
                                     </div>
                                     <div className="-mt-12 px-8 grid grid-cols-2 gap-2">
                                         <div className="bg-white rounded-md shadow px-4 py-2.5 dark:bg-[#060818]">
                                             <span className="flex justify-between items-center mb-4 dark:text-white">
-                                                Received
+                                                Today
                                                 <IconMenuInstaUsers className="w-4 h-4 text-success rotate-180" />
                                             </span>
-                                            <div className="btn w-full  py-1 text-base shadow-none border-0 bg-[#ebedf2] dark:bg-black text-[#515365] dark:text-[#bfc9d4]">$97.99</div>
+                                            <div className="btn w-full  py-1 text-base shadow-none border-0 bg-[#ebedf2] dark:bg-black text-[#515365] dark:text-[#bfc9d4]">979</div>
                                         </div>
                                         <div className="bg-white rounded-md shadow px-4 py-2.5 dark:bg-[#060818]">
                                             <span className="flex justify-between items-center mb-4 dark:text-white">
-                                                Spent
+                                                Monthly
                                                 <IconMenuInstaUsers className="w-4 h-4 text-danger" />
                                             </span>
-                                            <div className="btn w-full  py-1 text-base shadow-none border-0 bg-[#ebedf2] dark:bg-black text-[#515365] dark:text-[#bfc9d4]">$53.00</div>
+                                            <div className="btn w-full  py-1 text-base shadow-none border-0 bg-[#ebedf2] dark:bg-black text-[#515365] dark:text-[#bfc9d4]">530</div>
                                         </div>
                                     </div>
                                     <div className="p-5">
@@ -80,41 +96,52 @@ const Dashboard = () => {
                                             <div className="flex items-center justify-between">
                                                 <p className="text-[#515365] text-base font-semibold">Follows</p>
                                                 <p className="">
-                                                    <span className="font-semibold ">1566</span>
+                                                    <span className="font-semibold ">114</span>
                                                 </p>
                                             </div>
                                             <div className="flex items-center justify-between">
                                                 <p className="text-[#515365] text-base font-semibold">Unfollows</p>
                                                 <p className="">
-                                                    <span className="font-semibold ">1566</span>
+                                                    <span className="font-semibold ">342</span>
                                                 </p>
                                             </div>
                                             <div className="flex items-center justify-between">
                                                 <p className="text-[#515365] text-base font-semibold">Views</p>
                                                 <p className="">
-                                                    <span className="font-semibold ">1566</span>
+                                                    <span className="font-semibold ">297</span>
+                                                </p>
+                                            </div>
+                                            <div className="flex items-center justify-between">
+                                                <p className="text-[#515365] text-base font-semibold">Comments</p>
+                                                <p className="">
+                                                    <span className="font-semibold ">236</span>
                                                 </p>
                                             </div>
                                             <div className="flex items-center justify-between">
                                                 <p className="text-[#515365] text-base font-semibold">Directs</p>
                                                 <p className="">
-                                                    <span className="font-semibold ">1566</span>
+                                                    <span className="font-semibold ">751</span>
                                                 </p>
                                             </div>
                                             <div className="flex items-center justify-between">
                                                 <p className="text-[#515365] text-base font-semibold">Posts</p>
                                                 <p className="">
-                                                    <span className="font-semibold ">1566</span>
+                                                    <span className="font-semibold ">548</span>
                                                 </p>
                                             </div>
                                         </div>
-                                        <div className="text-center px-2 flex justify-around">
-                                            <button type="button" className="btn btn-secondary ltr:mr-2 rtl:ml-2">
-                                                View Details
+
+                                        <div className="text-center px-2 flex justify-around"><div className="relative inline-flex align-middle">
+                                            <button type="button" className="btn btn-success ltr:rounded-r-none rtl:rounded-l-none">
+                                                Start
                                             </button>
-                                            <button type="button" className="btn btn-success">
-                                                Pay Now $29.51
+                                            <button type="button" className="btn btn-dark rounded-none">
+                                                Settings
                                             </button>
+                                            <button type="button" className="btn btn-danger ltr:rounded-l-none rtl:rounded-r-none">
+                                                Stop
+                                            </button>
+                                        </div>
                                         </div>
                                     </div>
                                 </div>
